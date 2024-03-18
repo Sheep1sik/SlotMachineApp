b8f2a61081a84c73f8067bf3d7c1c7f36dec1be1
Sheep1sik <bohg6799@naver.com>
2024-03-18 16:31:04 +0900

    Feat: SetUp 및 Launch Screen 구현

---
+++ SlotMachine/SlotMachine/View/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SlotMachine
+//
+//  Created by 양원식 on 2024/03/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SlotMachine/SlotMachine/View/SlotMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotMachineApp.swift
+//  SlotMachine
+//
+//  Created by 양원식 on 2024/03/18.
+//
+
+import SwiftUI
+
+@main
+struct SlotMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
